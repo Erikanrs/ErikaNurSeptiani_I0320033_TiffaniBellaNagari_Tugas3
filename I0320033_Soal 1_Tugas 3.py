ce8dc6586ef1f4b3e4ba5566255a8b35603741d8
Erikanrs <nurseptianierika1@gmail.com>
2021-03-20 09:23:36 +0800

    Add files via upload

---
+++ I0320033_Soal 1_Tugas 3.py
@@ -0,0 +1,14 @@
+daftarteman=['afifah','Ana','RAhma','Taufiq','indri','Madon','Bintang','Ali','Ail']
+
+print("Daftar teman nomor 4,6dan 7 adalah",daftarteman[4],daftarteman[6],"dan",daftarteman[7])
+
+daftarteman[3]= 'Cantika'
+daftarteman[5]= 'Avissa'
+daftarteman[9]= 'Rafyan'
+
+daftarteman.append('Irgi')
+daftarteman.append('Gidion')
+
+print(daftarteman*2)
+
+print(len(daftarteman))
