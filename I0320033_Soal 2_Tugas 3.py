f371c20cac6d8c5a6be834bbc98f2f77acfbc585
Erikanrs <nurseptianierika1@gmail.com>
2021-03-20 09:49:55 +0800

    Add files via upload

---
+++ I0320033_Soal 2_Tugas 3.py
@@ -0,0 +1,12 @@
+#Membuat Dictionary
+
+dict={"Nama:", "Erika""hobi 1:", "Mendengarkan Musik","Hobi 2:"," Berenang","Hobi 3:""Ketoprak"Memasak","sosmed 1:","ig: @erikanrs_", "sosmed 2:","ig: @0rang.lain","sosmed 3:","twt:@bukankajom","lagu 1:","times to say goodbye","lagu 2:","00:00","lagu 3:","yours","makanan 1:","Seafood", "makanan 2:","Takoyaki", "makanan 3:","}
+
+#Mengubah salah satu hobi dan sosmed,menghapur 2 makanan, dan menambahkan 1 hobi
+dict["Hobi 1 :"]="ngabisin uang"
+dict["sosmed 3:"]="khaira.nzw"
+del dict["makanan 1:"]
+del dict["makanan 2:"]
+dict["Hobi 4:"="menari"]
+
+print(dict)
